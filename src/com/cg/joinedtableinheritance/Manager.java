bb369b23df51ab13eff8b73f5c438d1dbd22550f
swetha <shwethabs25@gmail.com>
2022-08-09 09:52:47 +0530

    first commit

---
+++ src/com/cg/joinedtableinheritance/Manager.java
@@ -0,0 +1,23 @@
+package com.cg.joinedtableinheritance;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="mgr_table")
+
+public class Manager extends Employee{
+	
+	private String DepartmentName;
+
+	public String getDepartmentName() {
+		return DepartmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		DepartmentName = departmentName;
+	}
+	
+	
+
+}
